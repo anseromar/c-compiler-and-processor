a4d946ff7269633b08f2b686180ad4f600ef52df
M. Dandin Brieuc <dandin@insa-07563>
2019-05-29 12:07:25 +0200

    datapath v1 traced

--- processor/Components/datapath.vhd
+++ processor/Components/datapath.vhd
@@ -4,8 +4,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity datapath is
 	-- Na: Generic size of an address
 	-- N: Generic size of the memory/register spaces
-	-- Nb:  Generic size of a bank & biggest size used (for stored data of any kind)
-	generic(Na: natural := 4 ; N: natural := 8 ; Nb: natural := 256);
+	generic(Na: natural := 4 ; N: natural := 8);
 	port (
 		CLK : IN  std_logic;
 		IP : IN std_logic_vector(N-1 downto 0);
@@ -89,7 +88,7 @@ architecture Structural of datapath is
 
 
 	-- Constant zero; used for comparisons
-	constant Zeros:  std_logic_vector(Nb-1 downto 0) := (others => '0');
+	constant Zeros:  std_logic_vector(4*N-1 downto 0) := (others => '0');
 
 	type instruction is record
 		Op, A, B, C: std_logic_vector(N-1 downto 0);
@@ -99,8 +98,8 @@ architecture Structural of datapath is
 	signal outBD, inP2, inP3, inP4, outP4: instruction;
 	signal back_to_RF: std_logic;
 	-- v1
---	signal inALU_AddrA: std_logic_vector(N-1 downto 0);
---	signal outP2_B: std_logic_vector(N-1 downto 0);
+--	signal inRF_AddrA: std_logic_vector(N-1 downto 0);
+--	signal outRF_A: std_logic_vector(N-1 downto 0);
 
 	begin
 		-- v0: Supports AFC
@@ -118,15 +117,15 @@ architecture Structural of datapath is
 --		-- v1: Support for AFC & COP
 --		IB:  instructions_bank	port map(CLK	,	IP		,		out_instr_bank);
 --		BD:  binary_decoder	port map(out_instr_bank		,		outBD.Op, outBD.A, outBD.B, outBD.C);
---		P1:  pipeline			port map(CLK	,	outBD.Op, outBD.A, outBD.B, outBD.C		,		inP2.Op, inP2.A, inP2.B, open);
---		RF:  register_file	port map(CLK, '0', back_to_RF	,	outP4.A(Na-1 downto 0), outP4.B, (others => '0'),	(others => '0')		,		open, open);
+--		P1:  pipeline			port map(CLK	,	outBD.Op, outBD.A, outBD.B, outBD.C		,		inP2.Op, inP2.A, inRF_AddrA, open);
+--		RF:  register_file	port map(CLK, '0', back_to_RF	,	outP4.A(Na-1 downto 0), outP4.B, inRF_AddrA,	(others => '0')		,		outRF_A, open);
 --		P2:	pipeline			port map(CLK	,	inP2.Op, inP2.A, inP2.B, Zeros(N-1 downto 0)		,		inP3.Op, inP3.A, inP3.B, open);
 --		ALU:	arithmetic_logic_unit	port map((others => '0'), (others => '0'), (others => '0')		,		open	,	open, open, open, open);
 --		P3:	pipeline			port map(CLK	,	inP3.Op, inP3.A, inP3.B, Zeros(N-1 downto 0)		,		inP4.Op, inP4.A, inP4.B, open);
 --		DB:	data_bank			port map('0', '0', '0'	,	(others => '0'), (others => '0')		,		open);
 --		P4:	pipeline			port map(CLK	,	inP4.Op, inP4.A, inP4.B, Zeros(N-1 downto 0)		,		outP4.Op, outP4.A, outP4.B, open);
 --		LCW:	combinatory_logic_W	port map(outP4.Op		,		back_to_RF);
---		MBP2:	multiplexer_reg_addr port map(inP2.Op, inALU_AddrA(Na-1 downto 0), outP2_B		,		inP2.B);
+--		MBP2:	multiplexer_reg_addr port map(inP2.Op, inRF_AddrA, outRF_A		,		inP2.B);
 		
 		-- v2: Added support for ADD, SOU & MUL
 		
