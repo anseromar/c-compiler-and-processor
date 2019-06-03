c5b272f50d6e7225956523659c637bf95c521fc1
M. Dandin Brieuc <dandin@insa-10582>
2019-06-03 18:27:58 +0200

    Fixed issue in multiplexer_UAL: same output either way until now...

--- processor/Components/multiplexer_UAL.vhd
+++ processor/Components/multiplexer_UAL.vhd
@@ -14,7 +14,7 @@ end multiplexer_UAL;
 
 architecture Behavioral of multiplexer_UAL is
 begin
-	Output <=	B when Op >= x"0001" AND Op <= x"0003"		-- Output takes the result from the ALU
+	Output <=	B when Op >= x"0001" AND Op <= x"0003"								-- Output takes the result from the ALU
 			else	A when (Op >= x"0005" AND Op <= x"0009")  OR Op = x"FFFF"	-- Bypass the ALU
-			else	x"FFFF";		-- Padding/reset/error
+			else	x"FFFF";		-- Padding/jump/reset/error
 end Behavioral;
