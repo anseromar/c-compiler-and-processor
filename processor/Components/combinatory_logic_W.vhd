31bd71bb0ce9efafaa911965371ad7f992fa3592
Brieuc Dandin <dandin@etud.insa-toulouse.fr>
2019-06-05 01:09:35 +0200

    Adapted Op conditions in combinatory_logic_W to JMPC & comparison operations. TODO: Same for all other CL & MUX.

--- processor/Components/combinatory_logic_W.vhd
+++ processor/Components/combinatory_logic_W.vhd
@@ -8,17 +8,18 @@ entity combinatory_logic_W is
 		-- Address of the memory space to start reading into
 		Op: in std_logic_vector(N-1 downto 0);
 		-- Reset; write
+		-- Reset if '1'
 		out_RST, Flag_W: out std_logic
 	);
 end combinatory_logic_W;
 
 architecture Behavioral of combinatory_logic_W is
 begin
-	-- Reset if '1'
+	-- Reset
 	out_RST <=	'1' when Op = x"0000"
 			else	'0';
-	Flag_W <=	-- ADD, MUL, SOU, DIV, COP, AFC, LOAD
-					'1' when Op >= x"0001" AND Op <= x"0007"		-- Write in register file
-					-- STORE, JMPC, RST, padding, error
+	Flag_W <=	-- ADD, MUL, SOU, DIV, COP, AFC, LOAD, EQU, INF, INFE, SUP, SUPE
+					'1' when (Op >= x"0001" AND Op <= x"0007") OR (Op >= x"0009" AND Op <= x"000D")		-- Write in register file
+					-- STORE, JMP, JMPC, RST, padding, error
 			else	'0';
 end Behavioral;
